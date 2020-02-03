0f3b3301af6c57e626e7db5da49bac4b334a36b4
volkmannp <60471729+volkmannp@users.noreply.github.com>
2020-02-03 15:05:31 +0000

    Update Demo ntbgraphics.R

--- Demo ntbgraphics.R
+++ Demo ntbgraphics.R
@@ -1,11 +1,13 @@
 ## February 2020
 ## Tcf4 data demo analysis for function "getexpdata" and "ploteachexp"
 
+library(devtools)
 library(ntbgraphics)
+devtools::load_all()
 
 ## get and modify data
-data.animal.joined <- getexpdata("/Users/paul/Documents/Einführung R/Wahlfach")
+data.animal.joined <- getexpdata(paste0(system.file("extdata/", package = "ntbgraphics", mustWork = T),"/"))
 
 ## loop through and plot list of experiments
 myexp <- c(as.list(colnames(data.animal.joined[, -(1:2)])))
-lapply(myexp, ntbgraphics::ploteachexp)
+lapply(myexp, ntbgraphics::ploteachexp, paste0(system.file("extdata/", package = "ntbgraphics", mustWork = T),"/"))
