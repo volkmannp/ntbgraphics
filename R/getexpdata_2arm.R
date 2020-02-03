7c4100e920288091add4c8867e89c8e3fd730e42
volkmannp <60471729+volkmannp@users.noreply.github.com>
2020-02-03 15:03:54 +0000

    new function to get data of 2 arm

---
+++ R/getexpdata_2arm.R
@@ -0,0 +1,39 @@
+#' @title Get data of NTB experiments
+#'
+#' @author Paul Volkmann
+#'
+#' @name getexpdata_2arm
+#'
+#' @description A function that imports a 2-arm GT NTB dataset and prepares the data for plotting and analysis.
+#'
+#' @param 'directory': file directory of Behavior and Animal List files
+#' (mind correct spelling of both files and directory!)
+#'
+#' @return prepared and joined dataframe of all animals and corresponding NTB experiments
+
+getexpdata_2arm <- function(directory) {
+
+  # load libraries
+  library(readxl)
+  library(tidyverse)
+  library(dplyr)
+
+  meta.data <-  read_excel(paste0(directory,"/Meta Behavior.xlsx"))
+  animal.list <-  read_excel(paste0(directory, "/Animal List.xlsx"))
+
+  data.animal.joined <-  animal.list %>%
+    # exclude NAs in Genotype
+    filter(Genotype!= 'NA') %>%
+    # join animals and behavior data
+    left_join(meta.data, by = c("RFID" = "Animal")) %>%
+    # change values from chr to num
+    dplyr::mutate_at(vars(Meanspeed:SerialLearn),(funs(as.numeric))) %>%
+    # select relevant columns
+    select(RFID, Genotype, Meanspeed:SerialLearn)
+
+  #get column/experiment names
+  myexp <- c(as.list(colnames(data.animal.joined[, -(1:2)])))
+
+  #return amended dataframe
+  return(data.animal.joined)
+}
