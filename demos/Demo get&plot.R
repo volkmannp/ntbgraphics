316597b0a82454da5108c022050ccd1502e889f9
volkmannp <60471729+volkmannp@users.noreply.github.com>
2020-02-04 15:14:54 +0000

    new file name

---
+++ demos/Demo get&plot.R
@@ -0,0 +1,13 @@
+## February 2020
+## Tcf4 data demo analysis for function "getexpdata" and "ploteachexp"
+## Plotting 4-arm experiments (by loop)
+
+## load libraries (and functions)
+library(ntbgraphics)
+
+## get and modify data
+data.animal.joined <- getexpdata(paste0(system.file("extdata/", package = "ntbgraphics", mustWork = T),"/"))
+
+## loop through and plot list of experiments
+myexp <- c(as.list(colnames(data.animal.joined[, -(1:2)])))
+lapply(myexp, ploteachexp, paste0(system.file("plots", package = "ntbgraphics", mustWork = T),"/"))
