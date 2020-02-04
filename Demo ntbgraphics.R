29f740ca2481407cc88c2fac6cc2a5bdc5045253
volkmannp <60471729+volkmannp@users.noreply.github.com>
2020-02-04 12:56:46 +0000

    Update documentation

--- Demo ntbgraphics.R
+++ Demo ntbgraphics.R
@@ -1,15 +1,13 @@
 ## February 2020
 ## Tcf4 data demo analysis for function "getexpdata" and "ploteachexp"
-## Plotting all experiments by loop
+## Plotting 4-arm experiments (by loop)
 
 ## load libraries (and functions)
-library(devtools)
 library(ntbgraphics)
-devtools::load_all()
 
 ## get and modify data
 data.animal.joined <- getexpdata(paste0(system.file("extdata/", package = "ntbgraphics", mustWork = T),"/"))
 
 ## loop through and plot list of experiments
 myexp <- c(as.list(colnames(data.animal.joined[, -(1:2)])))
-lapply(myexp, ntbgraphics::ploteachexp, paste0(system.file("extdata/", package = "ntbgraphics", mustWork = T),"/"))
+lapply(myexp, ntbgraphics::ploteachexp, paste0(system.file("plots", package = "ntbgraphics", mustWork = T),"/"))
--- Demo_2arm ntbgraphics.R
+++ Demo_2arm ntbgraphics.R
@@ -1,14 +1,12 @@
 ## February 2020
 ## Tcf4 data demo analysis for function "getexpdata_2arm" and "ploteachexp_2arm"
-## Plotting single experiments
+## Plotting 2-arm experiment (single experiments)
 
 ## load libraries (and functions)
-library(devtools)
 library(ntbgraphics)
-devtools::load_all()
 
 ## get and modify data
 data.animal.joined <- getexpdata_2arm(paste0(system.file("extdata/", package = "ntbgraphics", mustWork = T),"/"))
 
 ##  plot single experiments
-ploteachexp_2arm("Meanspeed", paste0(system.file("extdata/", package = "ntbgraphics", mustWork = T),"/"))
+ploteachexp_2arm("Meanspeed", paste0(system.file("plots", package = "ntbgraphics", mustWork = T),"/"))
