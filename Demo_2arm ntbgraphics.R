bc14dda19f2108dcc315d89ea11b2dc655680e94
volkmannp <60471729+volkmannp@users.noreply.github.com>
2020-02-04 11:17:55 +0000

    Correct and adapt explanations

--- Demo_2arm ntbgraphics.R
+++ Demo_2arm ntbgraphics.R
@@ -1,12 +1,14 @@
 ## February 2020
-## Tcf4 data demo analysis for function "getexpdata" and "ploteachexp"
+## Tcf4 data demo analysis for function "getexpdata_2arm" and "ploteachexp_2arm"
+## Plotting single experiments
 
+## load libraries (and functions)
 library(devtools)
 library(ntbgraphics)
 devtools::load_all()
 
-
 ## get and modify data
 data.animal.joined <- getexpdata_2arm(paste0(system.file("extdata/", package = "ntbgraphics", mustWork = T),"/"))
-## loop through and plot list of experiments
+
+##  plot single experiments
 ploteachexp_2arm("Meanspeed", paste0(system.file("extdata/", package = "ntbgraphics", mustWork = T),"/"))
