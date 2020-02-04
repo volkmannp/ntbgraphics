90ceb61dc2a14543bf31a17ed6ebdb3e3277850f
volkmannp <60471729+volkmannp@users.noreply.github.com>
2020-02-04 15:14:43 +0000

    new plotting pheatmap preparation

---
+++ demos/Demo pheatmap.R
@@ -0,0 +1,9 @@
+## February 2020
+## Tcf4 data demo analysis for function "___(pheatmap)"
+## Heatmap 4-arm experiment
+
+## load libraries (and functions)
+library(ntbgraphics)
+
+## get and modify data
+data.animal.joined <- getexpdata(paste0(system.file("extdata/", package = "ntbgraphics", mustWork = T),"/"))
