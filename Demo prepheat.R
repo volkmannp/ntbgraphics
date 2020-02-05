c5c231918b975a37d4a1c86d1974de801b08084a
volkmannp <60471729+volkmannp@users.noreply.github.com>
2020-02-05 12:20:28 +0000

    renamed, updated

---
+++ Demo prepheat.R
@@ -0,0 +1,13 @@
+## February 2020
+## Tcf4 data demo analysis for function "prepareheatmap"
+## Mapping 4-arm experiment
+
+## load libraries (and functions)
+library(ntbgraphics)
+
+## get and modify data
+data.animal.joined <- getexpdata(paste0(system.file("extdata", package = "ntbgraphics", mustWork = T),"/"))
+data.animal.matrix <- ntbgraphics::prepareheatmap(data.animal.joined)
+
+## map experiments
+pheatmap(data.animal.matrix)
